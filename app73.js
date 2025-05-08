7a798fe5a3cdb1d932a2d98d9b5f5960fa146768
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-05-08 13:24:36 +0530

    Add files via upload

---
+++ app73.js
@@ -0,0 +1,16 @@
+const calculator = {
+
+    num : 55,
+    add (a,b) {
+        return a+b;
+    },
+    sub (a,b) {
+        return a-b;
+    },
+    mul (a,b) {
+        return a*b;
+    },
+};
+
+
+
